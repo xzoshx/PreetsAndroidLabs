8078332c4b3691bc9dc2d0e5923b5308cbe07dc9
Preet Kaur <kaur0918@algonquinlive.com>
2023-05-10 18:45:22 -0400

    Initial Commit

---
+++ app/src/main/java/com/cst2335/Kaur0918/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cst2335.Kaur0918;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
